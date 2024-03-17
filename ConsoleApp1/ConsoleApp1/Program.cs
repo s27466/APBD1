da5feff38f42d9eda7b19692de3f508da86c7de3
s24666 <s27466@pjwstk.edu.pl>
2024-03-17 13:20:03 +0100

    Dodano average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mod1");
 Console.WriteLine("Mod2");
 Console.WriteLine("Mod3");
+
+int[] integers = new[] { 1, 2, 3, 4, 5 };
+Console.WriteLine(getAverage(integers));
+
+double getAverage(int[] ints)
+{
+    return ints.Average();
+}
